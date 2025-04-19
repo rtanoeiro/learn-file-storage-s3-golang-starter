1195e002af1640956bd1868ca4ac6a3b7bdb0988
rtanoeiro <ramontanoeiro@gmail.com>
2025-04-19 13:41:55 +1000

    updated method that updates thumbnail url in db

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -28,10 +30,68 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// "thumbnail" should match the HTML form input name
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+
+	fileData, errFile := io.ReadAll(file)
+
+	if errFile != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to read file", errFile)
+	}
+
+	videoMetadata, videoErr := cfg.db.GetVideo(videoID)
+
+	if videoErr != nil {
+		respondWithError(w, http.StatusBadRequest, "Video does not exisst", videoErr)
+		return
+	}
+
+	if userID != videoMetadata.UserID {
+		respondWithError(w, http.StatusUnauthorized, "Unable to read file", errFile)
+		return
+	}
+
+	videoThumbnail := thumbnail{
+		data:      fileData,
+		mediaType: mediaType,
+	}
+	videoThumbnails[videoID] = videoThumbnail
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID.String())
+	updateParams := database.Video{
+		ID:           videoMetadata.ID,
+		CreatedAt:    videoMetadata.CreatedAt,
+		UpdatedAt:    videoMetadata.UpdatedAt,
+		ThumbnailURL: &thumbnailURL,
+		VideoURL:     videoMetadata.VideoURL,
+		CreateVideoParams: database.CreateVideoParams{
+			Title:       videoMetadata.Title,
+			Description: videoMetadata.Description,
+			UserID:      userID,
+		},
+	}
+	errUpdate := cfg.db.UpdateVideo(updateParams)
+	if errUpdate != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to Update Video", errUpdate)
+		return
+	}
+
+	newVideoMetadata, newvideoErr := cfg.db.GetVideo(videoID)
+	if newvideoErr != nil {
+		respondWithError(w, http.StatusBadRequest, "Video does not exisst", videoErr)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, newVideoMetadata)
 }
